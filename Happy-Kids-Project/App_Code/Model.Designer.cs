17b01df84ceec9f89476561e49697e3466471773
NouraElRayan5 <Nourakhaled385@gmail.com>
2024-02-15 14:52:55 +0200

    noura

--- Happy-Kids-Project/App_Code/Model.Designer.cs
+++ Happy-Kids-Project/App_Code/Model.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\DevSpace\Happy-Kids-Project\Happy-Kids-Project\App_Code\Model.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Noura\OneDrive\Documents\My Web Sites\Happy-Kids-Project\Happy-Kids-Project\App_Code\Model.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
